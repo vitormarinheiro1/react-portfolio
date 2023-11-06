e93f5104e612a5bc0f070e5cd57b021251b186e4
unknown <vitormarinheiro9@gmail.com>
2023-11-06 05:48:24 -0300

    alterando json

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
   const projects = [
     {
       title: "Projeto 1",
-      description: "Descrição do projeto 1",
+      description: "Em andamento...",
       image:
         "https://images.unsplash.com/photo-1487014679447-9f8336841d58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1605&q=80",
       link: "#",
@@ -12,7 +12,7 @@ export default function Projects() {
     },
     {
       title: "Projeto 2",
-      description: "Descrição do projeto 2",
+      description: "Em andamento...",
       image:
         "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdlYnNpdGVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       link: "#",
@@ -20,7 +20,7 @@ export default function Projects() {
     },
     {
       title: "Projeto 3",
-      description: "Descrição do projeto 3",
+      description: "Em andamento...",
       image:
         "https://media.istockphoto.com/id/1305995602/photo/responsive-floating-responsive-design.jpg?b=1&s=170667a&w=0&k=20&c=uFqBz27v_B0UxcslnvZxd9c0D9eJAef7veRZf6b-v-A=",
       link: "#",
@@ -28,7 +28,7 @@ export default function Projects() {
     },
     {
       title: "Projeto 4",
-      description: "Descrição do projeto 4",
+      description: "Em andamento...",
       image:
         "https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29kaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       link: "#",
@@ -36,7 +36,7 @@ export default function Projects() {
     },
     {
       title: "Projeto 5",
-      description: "Descrição do projeto 5",
+      description: "Em andamento...",
       image:
         "https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9iaWxlJTIwYXBwfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       link: "#",
